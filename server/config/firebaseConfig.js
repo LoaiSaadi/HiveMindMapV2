e4234b0539611dc138fa4e880bf57df4c28c3fdd
samia2907 <samia.nabil.29.7@gmail.com>
2025-01-22 15:51:56 +0200

    fix the server error
    fix the server error

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 // החליפי את הקובץ למפתח השירות שלך שהורדת מ-Firebase Console
-//const serviceAccount = require("C:\\Users\\samia\\Documents\\MindMapProject\\server\\config\\mindmapapp-f4b64-firebase-adminsdk-3o8g9-53c1f26591.json");
+//const c = require("C:\\Users\\samia\\Documents\\MindMapProject\\server\\config\\mindmapapp-f4b64-firebase-adminsdk-3o8g9-53c1f26591.json");
 
 //require("C:\\Users\\esraa\\OneDrive\\Desktop\\mindmapapp-f4b64-firebase-adminsdk-3o8g9-80fc413c0c.json");
 admin.initializeApp({
